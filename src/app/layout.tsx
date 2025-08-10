b4c67637a0e1e3af121c66536116dfe4419f9b38
bishwa <108havocx108@gmail.com>
2025-08-10 14:49:26 +0545

    updated the layout with addition of themeprovider and fix hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
-
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
+import ThemeContextProvider from "@/components/ThemeContextProvider";
 
 export const metadata: Metadata = {
-  title: "Portfolio | Bishwa",
+  title: "Gurung Bishwa | Web Dev",
   description: "portfolio website build in next js and shadcn UI",
 };
 
@@ -13,9 +16,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
+    //to fix hydration error suppressHydrationWarning is used
+    <html
+      lang="en"
+      className={`${GeistSans.variable} ${GeistMono.variable}`}
+      suppressHydrationWarning
+    >
+      <body suppressHydrationWarning={true}>
+        <main className="border-b border-l border-r max-w-4xl  mx-auto">
+          <ThemeContextProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <NavBar />
+
+            {children}
+          </ThemeContextProvider>
+        </main>
       </body>
     </html>
   );
