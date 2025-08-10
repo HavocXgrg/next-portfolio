518ca02f203763cfecc7081e42253eab5d2dfb6e
bishwa <108havocx108@gmail.com>
2025-08-10 14:48:14 +0545

    updated some of the styling part

--- src/components/ThemeToggleBtn.tsx
+++ src/components/ThemeToggleBtn.tsx
@@ -21,10 +21,10 @@ export function ThemeToggleBtn() {
         <Button
           variant="outline"
           size="icon"
-          className="bg-white dark:bg-black text-black dark:text-white"
+          className=" mr-4 bg-white text-yellow-600 dark:bg-black  dark:text-blue-500 dark:hover:bg-gray-800 cursor-pointer"
         >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun className="rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
+          <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
           {/*sr- for reading only */}
         </Button>
