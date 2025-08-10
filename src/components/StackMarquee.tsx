cd64c3c32106f637e85b475c437b366cddc19794
bishwa <108havocx108@gmail.com>
2025-08-10 14:43:44 +0545

    added marquee component that renders the stack svg in a loop

---
+++ src/components/StackMarquee.tsx
@@ -0,0 +1,39 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const techStack = [
+  { name: "React", src: "/tech/react.svg" },
+  { name: "Next.js", src: "/tech/nextjs.svg" },
+  { name: "Tailwind", src: "/tech/tailwind.svg" },
+  { name: "HTML", src: "/tech/html.svg" },
+  { name: "Typescript", src: "/tech/typescript.svg" },
+  { name: "Nodejs", src: "/tech/nodejs.svg" },
+  { name: "Javascript ", src: "/tech/javascript.svg" },
+  { name: "Mongodb", src: "/tech/mongodb.svg" },
+];
+
+const StackMarquee = () => {
+  return (
+    <div className="rounded-xs overflow-hidden py-1 md:max-w-[460px] max-w-[360px] mx-auto ">
+      <Marquee loop={0} speed={55}>
+        <div className="flex-center gap-6">
+          {techStack.map((tech, index) => (
+            <div key={index} className="">
+              <Image
+                src={tech.src}
+                alt={tech.name}
+                width={40}
+                height={40}
+                className="object-contain"
+              />
+            </div>
+          ))}
+        </div>
+      </Marquee>
+    </div>
+  );
+};
+
+export default StackMarquee;
