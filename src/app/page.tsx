7b0079d6f3ec5c7721feb563a57f8d4e33697167
bishwa <108havocx108@gmail.com>
2025-08-10 14:49:58 +0545

    added components to render them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-
+import HeroSection from "@/components/HeroSection";
+import About from "@/components/About";
+import Project from "@/components/Project";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
     <div>
-      Portfolio
+      <HeroSection />
+      <About />
+      <Project />
+      <Contact />
     </div>
   );
 }
