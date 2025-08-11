8727f02816cea33a9c8fc765cf142bd08379c1bf
bishwa <108havocx108@gmail.com>
2025-08-11 16:07:52 +0545

    Added array of object containing data file

---
+++ src/Data/data.ts
@@ -0,0 +1,71 @@
+import { title } from "process";
+
+export const techStack = [
+  { name: "React", src: "/tech/react.svg" },
+  { name: "Next.js", src: "/tech/nextjs.svg" },
+  { name: "Tailwind", src: "/tech/tailwind.svg" },
+  { name: "HTML", src: "/tech/html.svg" },
+  { name: "Typescript", src: "/tech/typescript.svg" },
+  { name: "Nodejs", src: "/tech/nodejs.svg" },
+  { name: "Javascript ", src: "/tech/javascript.svg" },
+  { name: "Mongodb", src: "/tech/mongodb.svg" },
+];
+
+
+export const techStackHoverEffect = [
+  {
+    title: "React",
+    description: "A JavaScript library for building user interfaces.",
+    link: "https://reactjs.org",
+    src: "/tech/react.svg",
+  },
+  {
+    title: "Next.js",
+    description: "A React framework for server-side rendering and static sites.",
+    link: "https://nextjs.org",
+    src: "/tech/nextjs.svg",
+  },
+  {
+    title: "Tailwind",
+    description: "A utility-first CSS framework for rapid UI development.",
+    link: "https://tailwindcss.com",
+    src: "/tech/tailwind.svg",
+  },
+  {
+    title: "HTML 5",
+    description: "The standard markup language for creating web pages.",
+    link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+    src: "/tech/html.svg",
+  },
+  {
+    title: "TypeScript",
+    description: "A typed superset of JavaScript that enhances scalability.",
+    link: "https://www.typescriptlang.org",
+    src: "/tech/typescript.svg",
+  },
+  {
+    title: "Node.js",
+    description: "A JavaScript runtime for building server-side applications.",
+    link: "https://nodejs.org",
+    src: "/tech/nodejs.svg",
+  },
+  {
+    title: "JavaScript",
+    description: "A popular language for web development.",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    src: "/tech/javascript.svg",
+  },
+  {
+    title: "MongoDB",
+    description: "A NoSQL database for modern, scalable applications.",
+    link: "https://www.mongodb.com",
+    src: "/tech/mongodb.svg",
+  },
+
+  {
+    title: "Framer-Motion",
+    description: "A JS library for animations in React applications.",
+    link: "https://www.framer.com/motion/",
+    src:"/tech/framer-motion.svg"
+  }
+];
