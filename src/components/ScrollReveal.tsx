a1145dd25fe266f29d1004746c9bacbbc5441170
bishwa <108havocx108@gmail.com>
2025-08-11 16:04:28 +0545

    added a scrollreveal component that serves as an animation function while InView

---
+++ src/components/ScrollReveal.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import { easeInOut, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+interface ScrollRevealProps {
+  children: ReactNode;
+  className?: string;
+  delay?: number; //optional as it is for staggered animations
+}
+const ScrollReveal = ({
+  children,
+  className = "",
+  delay = 0,
+}: ScrollRevealProps) => {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0.1,
+  });
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 50 }}
+      animate={inView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.8, ease: "easeInOut", delay }}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default ScrollReveal;
